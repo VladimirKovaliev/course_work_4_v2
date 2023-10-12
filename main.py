296667d9d0bc3b99f1b424e1046663c630e38655
VladimirKovaliev <kovaliev.v@list.ru>
2023-10-12 21:42:28 +0300

    realization user interaction with json

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import json
 from headhunter_api import HeadHunter_API
 from superjob_api import SuperJobAPI
 from save_to_json import Save_to_json
 import time
+
 def interacion_with_user():
     while True:
+
         set_platform = input('На какой платформе искать вакансии?\n'
                              '1 - для HeadHunter\n'
                              '2 - для SuperJob\n'
@@ -18,13 +21,13 @@ def interacion_with_user():
                             '1 - да \n'
                             '2 - нет \n')
         if user_choise == '1':
-            user_filter = input('По какому фильтру подбирать вакансии?')
+            user_filter = input('По какому фильтру подбирать вакансии? ')
         print('Идет поиск, подождите', end=' ')
         for _ in range(3):
             time.sleep(1)
             print('.', end='', flush=True)
         print()
-        json = Save_to_json()
+
 
         if set_platform == '1':
             '''Поиск вакансий на HH'''
@@ -44,8 +47,6 @@ def interacion_with_user():
             sj = SuperJobAPI(vacancy)
             data = sj.get_vacancies()
             sorted_vacancies = sj.get_vacancy_info(data)
-            json.add_vacancy(sorted_vacancies)
-            json.save_to_file()
             for vacancy in sorted_vacancies:
                 print(f'{vacancy["name"]}, {vacancy["salary"]}, ID вакансии : {vacancy["id"]}, ({vacancy["location"]}) Опыт работы: {vacancy["experience"]} URL: {vacancy["link"]}')
 
@@ -68,8 +69,71 @@ def interacion_with_user():
 
         else:
             return('Неизвестный ввод')
+        while True:
+            user_choise = input('Сохранить результат в JSON файл?\n'
+                                '1 - да\n'
+                                '2 - нет\n'
+                                '3 - Зачем это нужно?\n'
+                                '4 - Завершить работу\n')
+            if user_choise == '1':
+                json = Save_to_json()
+                json.add_vacancy(sorted_vacancies)
+                json.save_to_file()
+                print('Данные сохранены.')
+                break
+            elif user_choise == '2':
+                break
+            elif user_choise == '3':
+                print('Это нужно для того, чтобы вы потом могли вывести, добавить, удалить или отсортировать вакансии')
+                print()
+            elif user_choise == '4':
+                break
+            else:
+                print('Неверный выбор. Попробуйте снова.')
+        user_choise = input('Продолжить работу?\n'
+                            '1 - Да\n'
+                            '2 - Завершить работу\n')
+        if user_choise == '1':
+            continue
+        return 'Завершаю работу'
+def user_interaction_with_json():
+    while True:
+        user_choise = input('Что вы хотите сделать?\n'
+                            '1 - Вывести содержимое JSON файла\n'
+                            '2 - Отсортировать вакансии в JSON файле по зарплате\n'
+                            '3 - Вывести топ N вакансий\n'
+                            '4 - Завершить работу\n')
+        if user_choise == '1':
+            print('Вывожу содержимое', end=' ')
+            for _ in range(3):
+                time.sleep(0.3)
+                print('.', end='', flush=True)
+            print()
+            with open('result.json', 'r') as file:
+                data = file.read()
+                parsed_data = json.loads(data)
+                for job in parsed_data:
+                    print(job['name'], job['salary'], job.get('location', 'Локация не указана'), job.get('experience', 'Опыт не нужен'), job.get('link', 'Ссылка отсутствует'))
+        if user_choise == '2':
+            pass
+            # Дальше идет сортировка, вывод топ вакансий, сохранение в JSON, поиск по ключевым словам, сортировка по зп
 
-        # Дальше идет сортировка, вывод топ вакансий, сохранение в JSON, поиск по ключевым словам, сортировка по зп
 
-go = interacion_with_user()
-print(go)
+
+
+greetings = input(f'Приветствую! Чем займемся? \n'
+                  f'1 - Поиск вакансий\n'
+                  f'2 - Работа с существующим JSON файлом ')
+if greetings == '1':
+    go = interacion_with_user()
+    print(go)
+elif greetings == '2':
+    go = user_interaction_with_json()
+    print(go)
+
+
+# with open('result.json', 'r') as file:
+#     data = file.read()
+#     parsed_data = json.loads(data)
+#     for job in parsed_data:
+#         print(job['name'], job['salary'], job['location'], job['experience'], job['link'])
