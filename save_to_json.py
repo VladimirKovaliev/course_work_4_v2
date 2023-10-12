34e109a8eaf7ec5c585cc02dbc1741cf4b58a4c0
VladimirKovaliev <kovaliev.v@list.ru>
2023-10-12 21:42:39 +0300

    fixes

--- save_to_json.py
+++ save_to_json.py
@@ -21,25 +21,22 @@ class MixinSave:
             json.dump(Save_to_json.list_to_save, fp=file)
 
 class Save_to_json(AbstractSaveToJSON, MixinSave):
-    """Класс для работы с вакансиями с JSON"""
+    """Класс для работы с вакансиями с jSOB"""
     PATH = 'result.json'
     list_to_save = []
     parser_list1 = []
     parser_list2 = []
     instance_list = []
 
-    def add_vacancy(self, vacancy) -> None:
+    def add_vacancy(self, vacancies) -> None:
         """Функция для сохранения вакансий в JSON файл"""
         Save_to_json.parser_list1.extend(Save_to_json.parser_list2)
         if os.path.exists('result.json'):
             with open(Save_to_json.PATH, 'r') as file:
                 if open(Save_to_json.PATH, 'r').read():
                     saved_vacations = json.loads(file.read())
-                    Save_to_json.list_to_save += saved_vacations  # добавляем сохраненные вакансии
-        for instance in Save_to_json.parser_list1:
-            if 'id' in instance.keys() and int(instance['id'] == int(vacancy.id)):
-                if instance not in Save_to_json.list_to_save:  # проверяем, нет ли такой вакансии уже в списке
-                    Save_to_json.list_to_save.append(instance)
+                    Save_to_json.list_to_save += saved_vacations
+        Save_to_json.list_to_save += vacancies  # добавление списка вакансий
 
     def get_vacancies_by_salary(self, salary: int) -> None:
         """Функция для просмотра сохраненных вакансий из JSON файла"""
