d3212aa9720d68854b2082cdb885d61d5d47f377
VladimirKovaliev <kovaliev.v@list.ru>
2023-10-13 15:01:55 +0300

    add new func filter_by_salary, get_top_vacancies, print_vacancies

---
+++ utils.py
@@ -0,0 +1,75 @@
+import json
+
+# def sort_vacancies(filtered_vacancies: list) -> list:
+#     """Сортировка вакансий по зп"""
+#     new_list = []
+#     for instance in filtered_vacancies:
+#         if instance.payment is None:
+#             del filtered_vacancies[filtered_vacancies.index(instance)]
+#     while filtered_vacancies:
+#         max_scale = filtered_vacancies[0].payment
+#         max_scale_exz = filtered_vacancies[0]
+#         for instance in filtered_vacancies:
+#             if instance.payment is not None and instance.payment  != 0:
+#                 if instance.payment > max_scale:
+#                     max_scale = instance.payment
+#                     max_scale_exz = instance
+#         else:
+#             new_list.append(max_scale_exz)
+#             del filtered_vacancies[filtered_vacancies.index(max_scale_exz)]
+#     return new_list
+
+import json
+
+def filter_by_salary(json_file, min_salary):
+    with open(json_file) as f:
+        data = json.load(f)
+    filtered_data = []
+    for job in data:
+        if job['salary'].startswith('Зарплата от'):
+            try:
+                salary_from = int(job['salary'].split()[2])
+                if salary_from >= min_salary:
+                    filtered_data.append(job)
+            except ValueError:
+                pass
+        else:
+            try:
+                salary = int(job['salary'].split()[0])
+                if salary >= min_salary:
+                    filtered_data.append(job)
+            except ValueError:
+                pass
+    return filtered_data
+#
+def get_top_vacancies(json_file, vacancies_count):
+    with open(json_file) as f:
+        data = json.load(f)
+    sorted_vacancies = []
+    for vacancy in data:
+        if vacancy['salary'].startswith('Зарплата от'):
+            salary = int(vacancy['salary'].split()[2])
+            sorted_vacancies.append({'name': vacancy['name'],
+                                     'salary': salary,
+                                     'location': vacancy.get('location', 'Локация не указана'),
+                                     'experience': vacancy.get('experience', 'Опыт не нужен'),
+                                     'link': vacancy.get('link', 'Ссылка отсутствует')})
+        else:
+            continue
+
+    sorted_vacancies = sorted(sorted_vacancies, key=lambda x: x['salary'], reverse=True)
+    return sorted_vacancies[:vacancies_count]
+
+# def get_top_vacancies(json_file, vacancies_count):
+#     with open(json_file) as f:
+#         data = json.load(f)
+#     sorted_vacancies = sorted(data, key=lambda x: x['salary'], reverse=True)
+#     return sorted_vacancies[:vacancies_count]
+
+def print_vacancies(vacancies):
+    for job in vacancies:
+        print(f"{job.get('name', 'Название вакансии не указано')},"
+              f" Зарплата: {job['salary']},"
+              f" Локация: {job.get('location', 'Локация не указана')},"
+              f" Опыт: {job.get('experience', 'Опыт не нужен')},"
+              f" Ссылка: {job.get('link', 'Ссылка отсутствует')}")
